b3937a5e4e2a717e6b5474298cc145e8f8326895
Horizon <zhr77fth@gmail.com>
2021-04-07 16:03:44 +0430

    init vue project

---
+++ src/components/Form.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div class="center form-container">
+    <h2 class="section-name your-name">Your Name</h2>
+    <input1 v-model="name" label="Name" :required="true" id="name" />
+    <input1 v-model="lastName" label="Last name" :required="true" id="lastname" />
+    <input1 v-model="name" label="Name" :required="true" id="name" />
+    <input1 v-model="name" label="Name" :required="true" id="name" />
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuesax from 'vuesax'
+Vue.use(Vuesax, {
+    // options here
+  })
+import input1 from './formComponent/myInput1.vue'
+    export default {
+      data:() => ({
+        value: ''
+      }),
+    components: {
+      input1
+  }
+    }
+</script>
+
+<style>
+.form-container{
+display: flex;
+justify-content: space-between;
+flex-wrap: wrap;
+width: 70vw;
+}
+.section-name{
+width: 100%;
+text-align: left;
+}
+</style>
---
+++ src/components/formComponent/myInput1.vue
@@ -0,0 +1,54 @@
+<template>
+<div id="input">
+      <vs-input
+        class="input"
+        type="text"
+        :id="id"
+        :label-placeholder="label"
+        :value="value"
+        :required="required"
+        v-on:input="updateValue($event.target.value)"
+      />
+      </div>
+</template>
+
+<script>  export default {
+    props: {
+      id: {
+        type: String
+      },
+      label: {
+        type: String,
+        required: true
+      },
+      value: {
+        type: String
+      },
+      required: {
+        type: Boolean,
+        default: false
+      }
+    },
+
+    methods: {
+      updateValue: function (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+</script>
+
+<style>
+#input{
+  width: 45%;
+}
+.input{
+  border: 0;
+  margin: 18px;
+  border-bottom: 3px solid rgb(12, 32, 71);
+  background-color: white;
+}
+.vs-input{
+  background-color: white;
+}
+</style>
